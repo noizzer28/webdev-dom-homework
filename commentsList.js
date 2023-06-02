54414c4945715a62c131a02ba724944cfd8db89d
noizzer28 <colosova28@yandex.ru>
2023-06-02 21:13:43 +0300

    attempt #1

---
+++ commentsList.js
@@ -0,0 +1,26 @@
+import { formatDate } from "./index.js";
+export const getListComments = (comment, index) => {
+  return`<li class="comment" data-index="${index}">
+  <div class="comment-header">
+    <div>${comment.name}
+        </div>
+    <div>${comment.date}</div>
+  </div>
+  <div class="comment-body">
+    <div class="comment-text">
+      ${comment.text
+ .replaceAll("&", "&amp;")
+ .replaceAll("<", "&lt;")
+ .replaceAll(">", "&gt;")
+ .replaceAll('"', "&quot;")}
+    </div>
+  </div>
+  <div class="comment-footer">
+    <div class="likes">
+      <span class="likes-counter">${comment.likes}</span>
+      <button data-index="${index}" id="likes-button" class="like-button ${comment.isLiked ? "-active-like" : ""} ${comment.isLikeLoading ? "-loading-like" : ""}"></button>
+    </div>
+  </div>
+</li>`
+}
+
